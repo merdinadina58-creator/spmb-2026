9c3440a7e4a60b605c09b23202bf8a384e08a8b8
Z User <z@container>
2026-05-22 07:22:02 +0000

    7e7ea014-a902-4b10-8709-7ec581218649

--- start-neon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export DATABASE_URL="postgresql://neondb_owner:npg_lJQ5pe2ayVGH@ep-bitter-salad-ao7imaxp-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-echo "DATABASE_URL=$DATABASE_URL" > /home/z/my-project/.env
-exec ./node_modules/.bin/next dev -p 3000
